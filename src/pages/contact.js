bb1ad9474f15be6d5367a22da68b23db0ff9be8a
onni82 <onni.bucht@me.com>
2021-12-08 20:33:26 +0100

    updated link behaviour on the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ export default function Contact() {
           <Header headerText="Contact" />
           <p>Send us a message!</p>
           <p>Email: <a href="mailto:emanuel.bucht@icloud.com">emanuel.bucht@icloud.com</a></p>
-          <p>Instagram: <a href="https://instagram.com/onni82">@onni82</a> &amp; <a href="https://instagram.com/onbprod">@onbprod</a></p>
+          <p>Instagram: <a href="https://instagram.com/onni82" target="_blank">@onni82</a> &amp; <a href="https://instagram.com/onbprod" target="_blank">@onbprod</a></p>
         </div>
       </Layout>
     </div>
